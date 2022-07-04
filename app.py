5ec1818fe0916c39d74bd78c7c02221b51a208d5
bicky <bickybong@gmail.com>
2022-07-04 15:52:15 +0800

    updated flask

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+items = [2,5,2,3]
 
 @app.route("/")
 def hello_world():
     return "<h1>Hello, World!</h1>"
+
+@app.route("/items")
+def iitems():
+    return jsonify(items=items)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
